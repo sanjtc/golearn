3917b3bb89461728131d90f77f8800213caf8b48
pantskun <wangxin_tongcheng@live.com>
2020-09-16 18:14:49 -0700

    update

--- customEtcdclt/customEtcdclt.go
+++ customEtcdclt/customEtcdclt.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"time"
 
 	"github.com/coreos/etcd/clientv3"
 )
+
+const httpMode = true
 
 var config clientv3.Config = clientv3.Config{
 	Endpoints:   []string{"127.0.0.1:2379"},
@@ -12,8 +15,11 @@ var config clientv3.Config = clientv3.Config{
 }
 
 func main() {
-	// flag.Parse()
-	// argv := flag.Args()
-	// ParseCommand(argv)
-	StartHTTPListen(":8080")
+	if httpMode {
+		StartHTTPListen(":8080")
+	} else {
+		flag.Parse()
+		argv := flag.Args()
+		ParseCommand(argv)
+	}
 }
