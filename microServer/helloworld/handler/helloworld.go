f73c5daf96480ce7e3c884ba84d3a661e2c08e21
pantskun <wangxin_tongcheng@live.com>
2020-09-29 10:39:46 +0800

    update

--- microServer/helloworld/handler/helloworld.go
+++ microServer/helloworld/handler/helloworld.go
@@ -12,6 +12,8 @@ type Helloworld struct{}
 
 func (e *Helloworld) Call(ctx context.Context, req *helloworld.Request, rsp *helloworld.Response) error {
 	logger.Info("Received Helloworld.Call request")
+
 	rsp.Msg = "Hello " + req.Name
+
 	return nil
 }
